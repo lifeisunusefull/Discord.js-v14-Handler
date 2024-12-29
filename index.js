8a9afcf665df0ec793b0b98428c7844cd4404dea
AmtiX <ameencaft@gmail.com>
2024-12-29 23:52:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
- GatewayIntentBits.MessageContent,
+    GatewayIntentBits.MessageContent
   ],
 });
 
